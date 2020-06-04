8575ca89e01c9f8717f38f4561600b40bf47c4d7
Adarsh <itsadarsh8@gmail.com>
2020-06-04 15:38:44 +0530

    added recycler view

--- app/src/main/java/com/example/eventbuzz/Adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/eventbuzz/Adapter/RecyclerViewAdapter.java
@@ -1,4 +1,60 @@
 package com.example.eventbuzz.Adapter;
 
-public class RecyclerViewAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.eventbuzz.POJO.EventPojo;
+import com.example.eventbuzz.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+    ArrayList<EventPojo> mEventPojoArrayList=new ArrayList<>();
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_view_holder, parent, false);
+        return new MyViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.concertName.setText(mEventPojoArrayList.get(position).getConcertName());
+        holder.startDate.setText(mEventPojoArrayList.get(position).getStartDate());
+        holder.endDate.setText(mEventPojoArrayList.get(position).getEndDate());
+        Picasso.get().load(mEventPojoArrayList.get(position).getImageUrl()).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mEventPojoArrayList.size();
+    }
+
+    public void updateEventList(ArrayList<EventPojo> eventPojoArrayList){
+        mEventPojoArrayList=eventPojoArrayList;
+        notifyDataSetChanged();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageView;
+        TextView concertName;
+        TextView startDate;
+        TextView endDate;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView=itemView.findViewById(R.id.imageView);
+            concertName=itemView.findViewById(R.id.concertTitle);
+            startDate=itemView.findViewById(R.id.startDate);
+            endDate=itemView.findViewById(R.id.endDate);
+
+        }
+    }
 }
--- app/src/main/java/com/example/eventbuzz/MainActivity.java
+++ app/src/main/java/com/example/eventbuzz/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.eventbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.eventbuzz.Adapter.RecyclerViewAdapter;
 import com.example.eventbuzz.POJO.EventPojo;
 import com.example.eventbuzz.Utility.JsonUtil;
 import com.example.eventbuzz.Utility.NetworkUtil;
@@ -15,11 +19,18 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private static String LOG_Error=MainActivity.class.getSimpleName();
     private USAsyncTask mUSAsyncTask;
+    private  RecyclerViewAdapter recyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView=findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewAdapter =new RecyclerViewAdapter();
+        recyclerView.setAdapter(recyclerViewAdapter);
+
         mUSAsyncTask=new USAsyncTask();
         mUSAsyncTask.execute(getResources().getString(R.string.US_Link));
     }
@@ -35,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String json) {
            ArrayList<EventPojo> eventPojoArrayList= JsonUtil.extractFeaturesFromJson(json);
+           recyclerViewAdapter.updateEventList(eventPojoArrayList);
             super.onPostExecute(json);
         }
     }
